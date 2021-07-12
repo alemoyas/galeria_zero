e0e28bb455d1633a9941994414d0c191af00306c
Ale <sistemarestobar@hotmail.com>
2021-07-11 20:11:18 -0400

    quo

--- core/views.py
+++ core/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from .models import Autor, Obra
-
-# Create your views here.
 
 
 def prueba(request):
+        picasso = Autor.objects.get(idautor=1)
+        
         autores = Autor.objects.all()
         obras =   Obra.objects.all()
 
-        datos = {"autores" : autores, "obras" : obras}
+
+        datos = {"picasso" : picasso, "autores" : autores, "obras" : obras}
         
+        
+
         return render(request, "core/prueba.html", datos)
+
+
+class prueba(DetailView):
+        obras =   Obra.objects.all()
+        model = obras 
+        context_object_name = "obras"
+        template_name = "core/prueba.html"
+
+
 
 def index(request):
         return render(request, "core/index.html")
