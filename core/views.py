330f29d77a63f8e870dbf1272baae1d2821e1723
Ale <sistemarestobar@hotmail.com>
2021-07-11 20:19:26 -0400

    ordinariamnete irterando por autor individual

--- core/views.py
+++ core/views.py
@@ -4,24 +4,27 @@ from .models import Autor, Obra
 
 
 def prueba(request):
+
+        #cosas de picasso
         picasso = Autor.objects.get(idautor=1)
-        
+        p1 = Obra.objects.get(idobra=1)
+        p2 = Obra.objects.get(idobra=2)
+        p3 = Obra.objects.get(idobra=3)
+        p4 = Obra.objects.get(idobra=4)
+        picasso_obras = [p1, p2, p3, p4]
+
+
+        #todos los autores todas las obras
         autores = Autor.objects.all()
         obras =   Obra.objects.all()
 
 
-        datos = {"picasso" : picasso, "autores" : autores, "obras" : obras}
+        datos = {"picasso" : picasso, "autores" : autores, "obras" : obras, "p1" : p1, "picasso_obras" : picasso_obras}
         
         
 
         return render(request, "core/prueba.html", datos)
 
-
-class prueba(DetailView):
-        obras =   Obra.objects.all()
-        model = obras 
-        context_object_name = "obras"
-        template_name = "core/prueba.html"
 
 
 
