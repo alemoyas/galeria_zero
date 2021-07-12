67a0c29950bdb2d1b2bf91a30dafb24213556e96
Ale <sistemarestobar@hotmail.com>
2021-07-11 22:33:48 -0400

    asfa

--- core/views.py
+++ core/views.py
@@ -7,11 +7,7 @@ def prueba(request):
 
         #cosas de picasso
         picasso = Autor.objects.get(idautor=1)
-        p1 = Obra.objects.get(idobra=1)
-        p2 = Obra.objects.get(idobra=2)
-        p3 = Obra.objects.get(idobra=3)
-        p4 = Obra.objects.get(idobra=4)
-        picasso_obras = [p1, p2, p3, p4]
+        picasso_obras = [Obra.objects.get(idobra=1), Obra.objects.get(idobra=2), Obra.objects.get(idobra=3), Obra.objects.get(idobra=4)]
 
 
         #todos los autores todas las obras
@@ -19,7 +15,7 @@ def prueba(request):
         obras =   Obra.objects.all()
 
 
-        datos = {"picasso" : picasso, "autores" : autores, "obras" : obras, "p1" : p1, "picasso_obras" : picasso_obras}
+        datos = {"picasso" : picasso, "autores" : autores, "obras" : obras, "picasso_obras" : picasso_obras}
         
         
 
@@ -38,11 +34,18 @@ def contacto(request):
         return render(request, "core/vistas_extras/contacto.html")
 
 
+def galeria_picasso(request):
+        o1 = Obra.objects.get(idobra=1)
+        o2 = Obra.objects.get(idobra=2)
+        o3 = Obra.objects.get(idobra=3)
+        o4 = Obra.objects.get(idobra=4)
+        
+        datos = {"o1" :o1, "o2" :o2, "o3" :o3, "o4" :o4}
+        return render(request, "core/galerias/galeria_picasso.html", datos)
 
+        
 def ficha_autorretrato_picasso(request):
         obra = Obra.objects.get(idobra=1)
         datos = {"obra" : obra}
         return render(request, "core/fichas/ficha_autorretrato_picasso.html",datos)
 
-def galeria_picasso(request):
-        return render(request, "core/galerias/galeria_picasso.html")
